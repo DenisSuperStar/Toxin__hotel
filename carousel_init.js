29de5bb6a313e1d293bcb9e35a5130ca3082cdd2
DenisSuperStar <62526089+DenisSuperStar@users.noreply.github.com>
2021-02-01 01:19:02 +0300

    Delete carousel_init.js

--- carousel_init.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $('.card-slider').slick({
-        arrows: false,
-        dots: true,
-        infinite: true,
-        speed: 0,
-        fade: true
-    });
-});
